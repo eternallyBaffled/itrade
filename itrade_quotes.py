a5c8e4af9e0dd1030f5483e1479885d063812b98
dgil <dgil@9d0f4225-f512-0410-881c-bdea70153025>
2006-12-03 14:45:26 +0000

    Be able again to change the live connector for a quote (regression)

--- itrade_quotes.py
+++ itrade_quotes.py
@@ -374,7 +374,9 @@ class Quote(object):
         self.m_pluginId = None
 
     def set_liveconnector(self,name):
+        #print 'set_liveconnector for ',self.m_market,self.m_list,QTAG_ANY,self.m_place,name
         conn = getLiveConnector(self.m_market,self.m_list,QTAG_ANY,self.m_place,name)
+        #print ' returns',conn
         if conn:
             self.m_liveconnector = conn
             self.m_pluginId = None
