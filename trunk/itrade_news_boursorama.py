3f38b95ddbbfae0aeca6ee37b7f827ba9ca5f22a
dgil <dgil@9d0f4225-f512-0410-881c-bdea70153025>
2006-12-11 13:36:39 +0000

    News : Boursorama News provider was buggy (SF bug 1612349) News : saving the cache with non ASCII char (SF bug 1613121) / UTF8

--- trunk/itrade_news_boursorama.py
+++ trunk/itrade_news_boursorama.py
@@ -173,7 +173,7 @@ class News_Boursorama(object):
                 print 'unable to connect'
             return
 
-        title = re.search('<tr>[ \t\n\r]+<td.*</td>[ \t\n\r]+</tr>',buf,re.IGNORECASE|re.MULTILINE|re.DOTALL)
+        title = re.search('<tr>[ \t\n\r]+<td.*</font></td>[ \t\n\r]+</tr>',buf,re.IGNORECASE|re.MULTILINE|re.DOTALL)
         if title:
             title = title.group()
         else:
@@ -182,6 +182,9 @@ class News_Boursorama(object):
         buf = re.search('<tr>[ \t\n\r]*<td>.*</table>',buf,re.IGNORECASE|re.MULTILINE|re.DOTALL)
         if buf:
             buf = buf.group()[:-8]
+            #print '----------------('
+            #print buf
+            #print ')----------------'
             page = "<html><body>" + "<br><a href=':back'>%s</a><H3>" % message('backtolist') + title + "</H3>" + buf + "<br><br><a href=':back'>%s</a>" % message('backtolist')  + "</body></html>"
 
             if html:
