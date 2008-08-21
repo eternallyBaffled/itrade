c2baa9bff3b5a0036ba02902ce99ee38f03faea1
dgil <dgil@9d0f4225-f512-0410-881c-bdea70153025>
2008-08-21 13:09:03 +0000

    Fix SF bug - error with last version matplotlib-0.98.1.win32-py2.5.exe

--- trunk/myfinance.py
+++ trunk/myfinance.py
@@ -4,27 +4,30 @@ financial data.   User contributions welcome!
 
 """
 #from __future__ import division
-import os, time, warnings, md5
+import os, time, warnings
 from urllib import urlopen
 
+try:
+    from hashlib import md5
+except ImportError:
+    from md5 import md5 #Deprecated in 2.5
 
 try: import datetime
 except ImportError:
     raise SystemExit('The finance module requires datetime support (python2.3)')
 
+import numpy as np
+
 from matplotlib import verbose, get_configdir
-from matplotlib.artist import Artist
-from matplotlib.dates import date2num, num2date
+from matplotlib.dates import date2num
 from matplotlib.cbook import Bunch
 from matplotlib.collections import LineCollection, PolyCollection
 from matplotlib.colors import colorConverter
 from matplotlib.lines import Line2D, TICKLEFT, TICKRIGHT
 from matplotlib.patches import Rectangle
-import matplotlib.numerix as nx
-from matplotlib.transforms import scale_transform, Value, zero, one, \
-     scale_sep_transform, blend_xy_sep_transform
+from matplotlib.transforms import Affine2D
 
-from pylab import gca
+
 
 configdir = get_configdir()
 cachedir = os.path.join(configdir, 'finance.cache')
@@ -44,13 +47,22 @@ def parse_yahoo_historical(fh, asobject=False, adjusted=True):
     results = []
 
     lines = fh.readlines()
+
+    datefmt = None
+
     for line in lines[1:]:
 
         vals = line.split(',')
 
         if len(vals)!=7: continue
         datestr = vals[0]
-        dt = datetime.date(*time.strptime(datestr, '%d-%b-%y')[:3])
+        if datefmt is None:
+            try:
+                datefmt = '%Y-%m-%d'
+                dt = datetime.date(*time.strptime(datestr, datefmt)[:3])
+            except ValueError:
+                datefmt = '%d-%b-%y'  # Old Yahoo--cached file?
+        dt = datetime.date(*time.strptime(datestr, datefmt)[:3])
         d = date2num(dt)
         open, high, low, close =  [float(val) for val in vals[1:5]]
         volume = int(vals[5])
@@ -67,7 +79,7 @@ def parse_yahoo_historical(fh, asobject=False, adjusted=True):
     if asobject:
         if len(results)==0: return None
         else:
-            date, open, close, high, low, volume = map(nx.asarray, zip(*results))
+            date, open, close, high, low, volume = map(np.asarray, zip(*results))
         return Bunch(date=date, open=open, close=close, high=high, low=low, volume=volume)
     else:
 
@@ -103,7 +115,7 @@ def fetch_historical_yahoo(ticker, date1, date2, cachename=None):
 
 
     if cachename is None:
-        cachename = os.path.join(cachedir, md5.md5(url).hexdigest())
+        cachename = os.path.join(cachedir, md5(url).hexdigest())
     if os.path.exists(cachename):
         fh = file(cachename)
         verbose.report('Using cachefile %s for %s'%(cachename, ticker))
@@ -324,9 +336,9 @@ def plot_day_summary2(ax, opens, closes, highs, lows, ticksize=4,
     offsetsClose = [ (i, close) for i, close in zip(xrange(len(closes)), closes) if close != -1 ]
 
 
-    scale = ax.figure.dpi * Value(1/72.0)
+    scale = ax.figure.dpi * (1.0/72.0)
 
-    tickTransform = scale_transform( scale, zero())
+    tickTransform = Affine2D().scale(scale, 0.0)
 
     r,g,b = colorConverter.to_rgb(colorup)
     colorup = r,g,b,1
@@ -371,10 +383,10 @@ def plot_day_summary2(ax, opens, closes, highs, lows, ticksize=4,
                                      )
     closeCollection.set_transform(tickTransform)
 
-    minx, maxx = (0, len(rangeSegments))
+    minpy, maxx = (0, len(rangeSegments))
     miny = min([low for low in lows if low !=-1])
     maxy = max([high for high in highs if high != -1])
-    corners = (minx, miny), (maxx, maxy)
+    corners = (minpy, miny), (maxx, maxy)
     ax.update_datalim(corners)
     ax.autoscale_view()
 
@@ -471,9 +483,7 @@ def candlestick2(ax, opens, closes, highs, lows, width=4,
     sx = ax.figure.dpi * Value(1/72.0)  # scale for points
     sy = (ax.bbox.ur().y() - ax.bbox.ll().y()) / (ax.viewLim.ur().y() - ax.viewLim.ll().y())
 
-    barTransform = scale_sep_transform(sx,sy)
-
-
+    barTransform = Affine2D().scale(sx,sy)
 
     r,g,b = colorConverter.to_rgb(colorup)
     colorup = r,g,b,alpha
@@ -573,7 +583,7 @@ def volume_overlay(ax, opens, closes, volumes,
     sx = ax.figure.dpi * Value(1/72.0)  # scale for points
     sy = (ax.bbox.ur().y() - ax.bbox.ll().y()) / (ax.viewLim.ur().y() - ax.viewLim.ll().y())
 
-    barTransform = scale_sep_transform(sx,sy)
+    barTransform = Affine2D().scale(sx,sy)
 
     offsetsBars = [ (i, 0) for i,v in enumerate(volumes) if v >= 0 ]
 
@@ -682,10 +692,10 @@ def volume_overlay3(ax, quotes,
 
     bars = [ ( (left, 0), (left, volume), (right, volume), (right, 0)) for d, open, close, high, low, volume in quotes]
 
-    sx = ax.figure.dpi * Value(1/72.0)  # scale for points
+    sx = ax.figure.dpi * (1.0/72.0)  # scale for points
     sy = (ax.bbox.ur().y() - ax.bbox.ll().y()) / (ax.viewLim.ur().y() - ax.viewLim.ll().y())
 
-    barTransform = scale_sep_transform(sx,sy)
+    barTransform = Affine2D().scale(sx,sy)
 
     dates = [d for d, open, close, high, low, volume in quotes]
     offsetsBars = [(d, 0) for d in dates]
@@ -706,15 +716,10 @@ def volume_overlay3(ax, quotes,
                                    )
     barCollection.set_transform(barTransform)
 
-
-
-
-
-
-    minx, maxx = (min(dates), max(dates))
+    minpy, maxx = (min(dates), max(dates))
     miny = 0
     maxy = max([volume for d, open, close, high, low, volume in quotes])
-    corners = (minx, miny), (maxx, maxy)
+    corners = (minpy, miny), (maxx, maxy)
     ax.update_datalim(corners)
     #print 'datalim', ax.dataLim.get_bounds()
     #print 'viewlim', ax.viewLim.get_bounds()
@@ -746,10 +751,10 @@ def index_bar(ax, vals,
 
     bars = [ ( (left, 0), (left, v), (right, v), (right, 0)) for v in vals if v != -1 ]
 
-    sx = ax.figure.dpi * Value(1/72.0)  # scale for points
+    sx = ax.figure.dpi * (1.0/72.0)  # scale for points
     sy = (ax.bbox.ur().y() - ax.bbox.ll().y()) / (ax.viewLim.ur().y() - ax.viewLim.ll().y())
 
-    barTransform = scale_sep_transform(sx,sy)
+    barTransform = Affine2D().scale(sx,sy)
 
     offsetsBars = [ (i, 0) for i,v in enumerate(vals) if v != -1 ]
 
@@ -764,10 +769,10 @@ def index_bar(ax, vals,
     barCollection.set_transform(barTransform)
 
 
-    minx, maxx = (0, len(offsetsBars))
+    minpy, maxx = (0, len(offsetsBars))
     miny = 0
     maxy = max([v for v in vals if v!=-1])
-    corners = (minx, miny), (maxx, maxy)
+    corners = (minpy, miny), (maxx, maxy)
     ax.update_datalim(corners)
     ax.autoscale_view()
 
