9581e0e7d55879622f49a009ffdc8401f4329385
m_leg <m_leg@9d0f4225-f512-0410-881c-bdea70153025>
2009-08-27 16:52:28 +0000

    New URL

--- trunk/ext/itrade_quotes_oslo.py
+++ trunk/ext/itrade_quotes_oslo.py
@@ -4,7 +4,7 @@
 # Project Name : iTrade
 # Module Name  : itrade_quotes_oslo.py
 #
-# Description: List of quotes from http://www.oslobors.no/ob/vis_aksjer
+# Description: List of quotes from http://www.oslobors.no/
 #
 # Developed for iTrade code (http://itrade.sourceforge.net).
 #
@@ -63,12 +63,14 @@ def Import_ListOfQuotes_OSLO(quotes,market='OSLO EXCHANGE',dlg=None,x=0):
                                 proxyAuth=itrade_config.proxyAuthentication)
 
     if market=='OSLO EXCHANGE':
-        url = "http://www.oslobors.no/ob/vis_aksjer"
+        starturl = 'http://www.oslobors.no/markedsaktivitet/stockIsinList?newt_isinList-stock_exch=ose&newt_isinList-stock_sort=aLONG_NAME&newt_isinList-stock_page='
+        endurl = '&newt__menuCtx=1.12'
+
     else:
         return False
 
     def splitLines(buf):
-        lines = string.split(buf, '\n')
+        lines = string.split(buf, 'Overview?')       
         lines = filter(lambda x:x, lines)
         def removeCarriage(s):
             if s[-1]=='\r':
@@ -78,61 +80,54 @@ def Import_ListOfQuotes_OSLO(quotes,market='OSLO EXCHANGE',dlg=None,x=0):
         lines = [removeCarriage(l) for l in lines]
         return lines
 
-    try:
-        data=connection.getDataFromUrl(url)
-    except:
-        debug('Import_ListOfQuotes_OSLO:unable to connect :-(')
-        return False
-
-    # returns the data
-    lines = splitLines(data)
-
-    count = 0
     nlines = 0
-    i = 1
+    endpage = 8
+    
+    select_page = ['1','2','3','4','5','6','7','8']
 
-    for line in lines:
+    for page in select_page:
+        
+        url = starturl + page + endurl
+  
+        try:
+            data=connection.getDataFromUrl(url)
+        except:
+            debug('Import_ListOfQuotes_OSLO:unable to connect :-(')
+            return False
+        
+        # returns the data
+        lines = splitLines(data)
 
-        #typical lines:
-        #<a href="/ob/aksje_kursutvikling?menu2show=1.1.2.1.&p_instrid=ticker.ose.ASC" target="_top">ABG Sundal Collier</a>
-        #</p>
-        #</td><td>
-        #<p style="text-align:left;">NO0003021909</p>
-        # extract data
+        #typical line
+        #newt__ticker=ASC" title="">ABG Sundal Collier Holding</a></td><td class="c1 o">NO0003021909</td><td class="c2 l">ASC</td></tr><tr id="manamind_isinList__stock_table_table_r2" class="r2"><td class="c0 f"><a href="
+        
+        for line in lines:
+   
+            if line.find('newt__ticker=')<> -1:
 
-        if i==1:
-            ch ='<a href="/ob/aksje_kursutvikling?menu2show=1.1.2.1.&p_instrid=ticker.ose.'
-            if ch == line[:len(ch)]:
-                i = 1 - i
-                ch = line.strip()
-                ch = ch[73:-4]
-                ticker = ch[:ch.index('"')]
-                name = ch[ch.index('>')+1:]
-                #print name,type(name)
-                name = name.replace('&amp;','&')
-                name = name.replace('&aelig;','ae')#Æ
-                name = name.replace('&oslash;','o')#ø
-                name = name.replace('&Oslash;','O')#Ø
-                #name = name.upper()
-                #print name
+                #partial activation of Progressbar
+                dlg.Update(x,'%s : %s / %s'%(market,page,endpage))
 
-        elif i==0:
-            ch ='<p style="text-align:left;">'
-            if ch == line[:len(ch)]:
-                i = 1 - i
-                ch = line.strip()
-                isin = ch[28:-4]
-                #print isin
+                ticker = line[line.index('newt__ticker=')+13:line.index('" title="">')]
 
-                # ok to proceed
+                if ticker == 'SAS+NOK' : ticker = 'SAS'
+                
+                name = line[line.index(' title="">')+10:line.index('</a></td><td')]
+
+                name = name.replace('ø','o')
+
+                isin = line[line.index('class="c1 o">')+13:line.index('</td><td class="c2 l">')]
+
+
+                #ok to proceed
 
                 quotes.addQuote(isin=isin,name=name, \
-                ticker=ticker,market='OSLO EXCHANGE',currency='NOK',place='OSL',country='NO')
-                count = count + 1
+                             ticker=ticker,market='OSLO EXCHANGE', \
+                            currency='NOK',place='OSL',country='NO')
 
                 nlines = nlines + 1
 
-    print 'Imported %d/%d lines from OSLO EXCHANGE data.' % (count,nlines)
+    print 'Imported %d lines from OSLO EXCHANGE data.' % (nlines)
 
     return True
 
