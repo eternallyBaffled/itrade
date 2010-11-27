dfc3698162831aaf76f43dad1f84b27f849d3751
m_leg <m_leg@9d0f4225-f512-0410-881c-bdea70153025>
2010-11-27 20:05:43 +0000

    Bug 3105971 - Only save the initial graph (without Hline, Vline...)

--- trunk/itrade_wxgraph.py
+++ trunk/itrade_wxgraph.py
@@ -465,12 +465,16 @@ class iTrade_wxToolbarGraph(wx.ToolBar):
         self.m_parent.OnToolTrash(event)
 
     def save(self,event):
-        filetypes = self.m_canvas._get_imagesave_wildcards()
-        dlg = wx.FileDialog(self.m_parent, message('save_to_file'), itrade_config.dirSnapshots, "", filetypes, wx.SAVE|wx.OVERWRITE_PROMPT|wx.CHANGE_DIR)
+        filetypes, exts, filter_index = self.m_canvas._get_imagesave_wildcards()
+        default_file = self.m_parent.m_quote.name() + "." + self.m_canvas.get_default_filetype()
+        dlg = wx.FileDialog(self.m_parent, message('save_to_file'),
+                            itrade_config.dirSnapshots,default_file,filetypes,
+                            wx.SAVE|wx.OVERWRITE_PROMPT|wx.CHANGE_DIR)
+        dlg.SetFilterIndex(filter_index)
         if dlg.ShowModal() == wx.ID_OK:
             dirname  = dlg.GetDirectory()
             filename = dlg.GetFilename()
-            debug('Save file dir:%s name:%s' % (dirname, filename))
+            debug('Save file dir:%s name:%s' % (dirname, filename), 3, self)
             self.m_canvas.print_figure(os.path.join(dirname, filename))
         dlg.Destroy()
 
